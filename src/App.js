2d28e72ce4496db766ab4f6d3fcf11fd983289a5
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2020-11-14 14:25:25 +0000

    inital test app built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
+import InputArea from "./components/InputArea";
+import ToDoItem from "./components/ToDoItem";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItems = (inputText) => {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    })
+  }
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItems} />
+      <div>
+        <ul>
+          {items.map((item,index) => (
+            <ToDoItem key={index} id={index} text={item} onChecked={deleteItem}/>
+        ))}
+        </ul>
+      </div>
+
+      </div>
   );
 }
 
